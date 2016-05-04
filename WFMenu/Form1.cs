a6e1737140924694b7300ea9a4dcd375d3df3aed
IliasN <iliascfpt@gmail.com>
2016-05-04 09:35:24 +0200

    Update Form1.cs

--- WFMenu/Form1.cs
+++ WFMenu/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +15,59 @@ namespace WFMenu
         public Form1()
         {
             InitializeComponent();
+        }
+
+        Menu menu;
+
+        private void btnBeef_Click(object sender, EventArgs e)
+        {
+            menu.AddBeef();
+            if (menu.NbBeef > 0)
+            {
+                btnBeef.Text = menu.NbBeef.ToString() + " : Roti de boeuf";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            menu = new Menu();
+        }
+
+        private void btnSpag_Click(object sender, EventArgs e)
+        {
+            menu.AddSpag();
+            if (menu.NbSpag > 0)
+            {
+                btnSpag.Text = menu.NbSpag.ToString() + " : Plat de spaghettis";
+            }
+        }
+
+        private void btnTruite_Click(object sender, EventArgs e)
+        {
+            menu.AddTruite();
+            if (menu.NbTruite > 0)
+            {
+                btnTruite.Text = menu.NbTruite.ToString() + " : Truite aux amandes";
+            }
+        }
+
+        private void btnDessert_Click(object sender, EventArgs e)
+        {
+            menu.AddDessert();
+            if (menu.NbDessert > 0)
+            {
+                btnDessert.Text = menu.NbDessert.ToString() + " : Dessert";
+            }
+        }
+
+        private void btnDone_Click(object sender, EventArgs e)
+        {
+            tbxResult.Text += menu.ToString();
+            menu = new Menu();
+            btnDessert.Text ="Dessert";
+            btnTruite.Text = "Truite aux amandes";
+            btnSpag.Text = "Plat de spaghettis";
+            btnBeef.Text = "Roti de boeuf";
         }
     }
 }
